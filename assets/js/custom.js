02b6fa907ecff87f7fbfe111b3de7f954ee423de
greattambino <devtambino@gmail.com>
2020-08-13 18:33:49 -0700

    add a loading spinner for all those with slow internet

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,15 @@
+/* Constants
+-----------------------------------------------*/
+  const $window = $(window);
+
+
+/* jQuery Pre loader
+-----------------------------------------------*/
+  $window.load(function() {
+    $('.preloader').fadeOut(1000);
+  });
+
+
 $(document).ready(function() {
 
   /* Home Slideshow Vegas
