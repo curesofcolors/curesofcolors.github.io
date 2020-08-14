79127d2dc6438d87ed45f52d580b6e3ea74ac6f3
greattambino <devtambino@gmail.com>
2020-08-13 22:46:11 -0700

    add fixed button to quickly scroll back to the top of the page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,32 @@
 /* Constants
 -----------------------------------------------*/
   const $window = $(window);
+  const $goTop = $('.go-top');
+
+
+/* Debounce event callbacks like smoothscroll
+-----------------------------------------------*/
+  const debounce = (func, wait, immediate) => {
+    var timeout;
+
+    return function executedFunction() {
+      var context = this;
+      var args = arguments;
+
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+
+      var callNow = immediate && !timeout;
+
+      clearTimeout(timeout);
+
+      timeout = setTimeout(later, wait);
+
+      if (callNow) func.apply(context, args);
+    };
+  };
 
 
 /* jQuery Pre loader
@@ -43,6 +69,21 @@ $(document).ready(function() {
         transition: [ 'zoomIn', ],
         animation: ['kenburns']
       });
+    });
+
+
+  /* Back to Top
+  -----------------------------------------------*/
+    const toggleGoTop = () => {
+      $(this).scrollTop() > 200 ? $goTop.fadeIn(200) : $goTop.fadeOut(200);
+    };
+
+    $window.scroll(debounce(toggleGoTop, 200));
+
+    // Animate the scroll to top
+    $goTop.click(function(event) {
+      event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
     });
 
 
