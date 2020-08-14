4f028119be76247bb961a06835f2c331a7a28b49
greattambino <devtambino@gmail.com>
2020-08-13 18:27:52 -0700

    add a cool background photo with vegas animation

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+
+  /* Home Slideshow Vegas
+  -----------------------------------------------*/
+    $(function() {
+      $('body').vegas({
+        slides: [{ src: 'assets/images/drawing-bg.png' }],
+        timer: false,
+        transition: [ 'zoomIn', ],
+        animation: ['kenburns']
+      });
+    });
+
+
   /* Wow js
   -----------------------------------------------*/
     new WOW({ mobile: false }).init();
